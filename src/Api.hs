812fcf66dded3080af781c8931ab8dba5e96ade7
Kratika Bansal <kratikabansal016@gmail.com>
2026-09-11 12:55:22 +0530

    Read PORT from environment for Render compatibility

--- src/Api.hs
+++ src/Api.hs
@@ -8,6 +8,7 @@ import Web.Scotty
 import Network.Wai.Middleware.Cors
 import Network.HTTP.Types (hContentType)
 import Data.Aeson (object, (.=))
+import System.Environment (lookupEnv)
 
 import Transaction (Transaction (..), TxnState (..))
 import Engine (evaluateTransaction, failedRuleNames)
@@ -19,16 +20,19 @@ corsPolicy = simpleCorsResourcePolicy
   }
 
 runApi :: IO ()
-runApi = scotty 3000 $ do
-  middleware (cors (const (Just corsPolicy)))
-  get "/" $ do
-    text "RuleForge API is running. POST a transaction to /evaluate"
-  post "/evaluate" $ do
-    txn <- jsonData :: ActionM Transaction
-    let state = evaluateTransaction txn
-        reasons = failedRuleNames txn
-    json (object
-      [ "txnId"   .= txnId txn
-      , "state"   .= show state
-      , "reasons" .= reasons
-      ])
+runApi = do
+  portStr <- lookupEnv "PORT"
+  let port = maybe 3000 read portStr
+  scotty port $ do
+    middleware (cors (const (Just corsPolicy)))
+    get "/" $ do
+      text "RuleForge API is running. POST a transaction to /evaluate"
+    post "/evaluate" $ do
+      txn <- jsonData :: ActionM Transaction
+      let state = evaluateTransaction txn
+          reasons = failedRuleNames txn
+      json (object
+        [ "txnId"   .= txnId txn
+        , "state"   .= show state
+        , "reasons" .= reasons
+        ])
